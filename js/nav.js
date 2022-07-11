467a34147d57840bb755f8fbda6b5dfae11cf513
Yuka Yamamoto <yamamoto_yuka@hotmail.com>
2022-07-11 14:20:19 -0700

    Organaized css and HTML files

--- js/nav.js
+++
@@ -1,7 +0,0 @@
-$(".hamburger").click(function () {
-  $(this).toggleClass("active");
-  $(".nav-links").toggleClass("open");
-  $(".nav-links li").each(function () {
-    $(this).toggleClass("listfade");
-  });
-});
